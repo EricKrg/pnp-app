4e42019f984e4abb7e6bc6e529f8e25befb4212c
Eric Krüger <ekrueger@codematix.de>
2021-02-23 20:32:10 +0100

    test change

--- backend/controller/app.py
+++ backend/controller/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return json.dumps({'msg': 'hi'})
 
 
 if __name__ == '__main__':
