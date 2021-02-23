f5e1385309953ad7cd3d88ebd65a5f6b88964ad8
MaxBrth <max.dennhorizon@gmail.com>
2021-02-23 21:41:51 +0100

    added new endpoint

--- backend/controller/app.py
+++ backend/controller/app.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print('test')
     return json.dumps({'msg': 'hi'})
+
+@app.route('/test')
+def test():
+    print('test')
+    return json.dumps({'msg': 'test'})
 
 
 if __name__ == '__main__':
